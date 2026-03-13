ed5d1dcbda8ce179040a4306d97d008c8124a244
= <=>
2026-03-13 06:32:33 +0300

    commit OrderService

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.food.ordering.system.order.service.domain.ports.output.repository;
+
+import com.food.ordering.system.order.service.domain.entity.Customer;
+
+import java.util.Optional;
+
+public interface CustomerRepository {
+    Optional<Customer> findCustomer(Customer customer);
+
+}
